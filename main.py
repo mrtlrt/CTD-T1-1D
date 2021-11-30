b4e02fd7e0b2ed3acfe29b231e4abfd7aff51861
mrtlrt <91262950+mrtlrt@users.noreply.github.com>
2021-11-30 13:43:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,9 +44,25 @@ countries_and_capitals = {
     }
 
 #todo: function to randomly pick 3 keys from a dictionary, then pick one key from those three keys for any length of that dictionary (no hardcoded values; should work on dictionaries with 1 or 8 or 9999 entries) - takes in DICT 1, returns one STRING chosen country, one LIST OF three STRINGS country options
+def place_picker():
 
+    import random
+    countries_and_capitals = {"Australia": "Canberra", "Belgium": "Brussels", "Canada": "Ottawa", "Denmark": "Copenhagen", "Egypt": "Cairo", "France": "Paris", "Germany": "Berlin", "Hungary": "Budapest", "India": "New Delhi", "Japan": "Tokyo", "South Korea": "Seoul", "Laos": "Vientiane", "Malaysia": "Kuala Lumpur", "New Zealand": "Wellington", "Poland": "Warsaw", "Russia": "Moscow"}
+    
+
+    country1, capital1 = random.choice(list(countries_and_capitals.items()))
+    country2, capital2 = random.choice(list(countries_and_capitals.items()))
+    country3, capital3 = random.choice(list(countries_and_capitals.items()))
+
+    random_countries_and_capitals = {country1:capital1, country2:capital2, country3:capital3}
+
+    final_random_country, final_random_capital = random.choice(list(random_countries_and_capitals.items()))
+
+    return (final_random_country, final_random_capital)
 #todo: function to randomly choose a city from the 1st dictionary with a specified country - takes in DICT 1 and one STRING chosen country, returns one STRING chosen city
-
+def random_city(countries_and_cities, chosen_country):
+    city_random_choice = random.choice(list(countries_and_cities[chosen_country]))
+    return city_random_choice
 #todo: function to check whether the city chosen is the capital of the country specified or not - takes in DICT 2 and two STRINGS chosen country and chosen city, if chosen city is capital returns one BOOLEAN TRUE, otherwise FALSE
 
 #todo: function to import a DICTIONARY from an external [scores.txt], then append an INTEGER to a LIST IN THE DICTIONARY where key = STRING player name, values = LIST of INTEGER previous scores - takes in one STRING playername, one INT score, writes to external file [scores.txt]
